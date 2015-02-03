f46e3bc75433ed98b24503224bcf12074f3f095c
Kyle Magnesen <kylewalkermagnesen@hotmail.com>
2015-02-03 09:42:21 -0600

    Added Signup ViewController

---
+++ Instagramo/SignupViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignupViewController.h
+//  Instagramo
+//
+//  Created by Kyle Magnesen on 2/3/15.
+//  Copyright (c) 2015 Syed, Kyle and JP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignupViewController : UIViewController
+
+@end
