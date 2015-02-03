2855bbec739859d71855cda06a39ce6f94cba7a3
Syed Amaanullah <syedamaanullah@Syeds-MacBook-Pro.local>
2015-02-03 16:24:28 -0600

    Login and Sign up functionality is coming along..

---
+++ Instagramo/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Instagramo
+//
+//  Created by Syed Amaanullah on 2/3/15.
+//  Copyright (c) 2015 Syed, Kyle and JP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
