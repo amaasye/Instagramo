3f6f29bba47b9937ec99b19109e7594e3f5b4452
JP Skowron <jpskowron@JP-Skowrons-MacBook-Pro.local>
2015-02-05 14:48:28 -0600

    cells

--- Instagramo/FollowersTableViewController.m
+++ Instagramo/FollowersTableViewController.m
@@ -11,35 +11,45 @@
 @interface FollowersTableViewController ()
 
 @property (strong, nonatomic) IBOutlet UITableView *followTableView;
+@property NSArray *userNameArray;
 @end
 
 @implementation FollowersTableViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+
+    cell.textLabel.text = [self.userNameArray objectAtIndex:indexPath.row];
+    NSLog(@"Text : %@", [self.userNameArray objectAtIndex:indexPath.row]);
+
+    
+
+    return cell;
 }
+
+
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    // Return the number of sections.
-    return 0;
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    // Return the number of rows in the section.
-    return 0;
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.userNameArray count];
 }
 
 /*
